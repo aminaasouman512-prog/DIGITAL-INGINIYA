8a0377a0ebc264f9ecd51337a671ad1b0610d600
aminaasouman512-prog <aminaasouman512@gmail.com>
2026-03-29 00:13:44 +0100

    style.css

---
+++ style.css
@@ -0,0 +1,43 @@
+<!DOTYPE html>
+<html lang="en">
+<head><
+<meta charset="UTF-8">
+<title>DIGETAL IGINIYARIN<title>
+<meta name="viwport" contt"=width=device-width, initial-scale=1.0">
+
+<!-- Link CSS -->
+<link rel="stylesheet" href="style.css">
+<head>
+<body>
+
+<header>
+<h1>DIGITAL INGENIYARIN</h1>
+<p>Marketplace na digital</p>
+</header>
+
+<main class="container">
+
+<div class="card">
+<h2>Barka da zuwa<h2></h2>
+<p>Wannan App din kasuwanci ne na siya dasiyarwa.</p>
+</div>
+
+<div class="card">
+<h3>Categories</h3>
+<ul>
+<li>Kayanwaya & hanu</li>
+<lo>kayaan gera</li>
+<li>Furniture</li>
+<li>Kaynwaya</li>
+<li>dababin sikirin</li>
+</ul>
+</div>
+
+<button id="startBtn">Fara Amfani</button>
+
+</main>
+
+<!-- Link js -->
+<script src="svtipt.js"></scripy>
+</body>
+</html>
